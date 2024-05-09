f91195d75efffd73f7b83bd5e08e6a267e858511
jerin-kunjumon <jkjerryomalloor@gmail.com>
2024-05-09 08:39:20 +0530

    Navbar Offers plan

---
+++ restaurant-using-react/src/App.js
@@ -0,0 +1,17 @@
+import Hero from './components/Hero.jsx'
+import Navbar from './components/Navbar.jsx';
+import Offers from './components/Offers.jsx';
+import Plan from './components/Plan.jsx';
+
+function App() {
+  return (
+    <div >
+      <Navbar />
+      <Hero />
+      <Offers />
+      <Plan />
+    </div>
+  );
+}
+
+export default App;
---
+++ restaurant-using-react/src/components/Plan.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Plan = () => {
+  return (
+    <div>
+        {/* Left Side */}
+      <div>
+        <img src='https://images.pexels.com/photos/240526/pexels-photo-240526.jpeg' alt='' />
+        <img src="https://images.pexels.com/photos/457878/pexels-photo-457878.jpeg" alt="" />
+        <img src="https://images.pexels.com/photos/240526/pexels-photo-240526.jpeg" alt="" />
+        <img src="https://images.pexels.com/photos/221471/pexels-photo-221471.jpeg" alt="" />
+        <img src="https://images.pexels.com/photos/50594/sea-bay-waterfront-beach-50594.jpeg" alt="" />
+      </div>
+      {/* Right Side */}
+      <div>
+        <h3>
+            Plan Your Next Trip
+        </h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, in?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet dolore est dolor unde nemo, temporibus mollitia. Porro quod quaerat recusandae cupiditate quos, magni excepturi ut iusto praesentium officiis neque quis.</p>
+        <div>
+            <button>Learn More</button>
+            <button>Learn More</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Plan
